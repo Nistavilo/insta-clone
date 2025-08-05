1aa6e4cea6cf09d51bbedc9f5d72831cc3111f47
Nistavilo <emirhankorkmaz275@gmail.com>
2025-08-06 00:09:58 +0300

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+
+import './App.css'
+
+function App() {
+return (
+  <div className="text-yellow-500 text-3xl font-bold">
+    Tailwind Çalışıyor!
+  </div>
+);}
+
+export default App
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='container'>
+        <div className='homepage'>
+            <div className="left">
+                <div className="posts">
+                    
+                </div>
+            </div>
+            <div className="right">
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
